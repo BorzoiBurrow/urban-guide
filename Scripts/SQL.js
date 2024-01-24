c4ea52217eccee75d1245dec87fce53abba44c39
Brad <Bradrockk@gmail.com>
2024-01-24 15:14:15 -0500

    continued work on creation of values for insertion

--- Scripts/SQL.js
+++ Scripts/SQL.js
@@ -1,13 +1,23 @@
-const { default: inquirer } = require("inquirer")
+const inquirer = require('inquirer');
+let entrynum = 0
 
 async function addvalue(sql, count) {
-let value = ''
 console.log(count)
-// inquirer.prompt([
+while (entrynum < count)(
+ inquirer.prompt([
+    {
+        type: "input",
+        name: "value",
+        message: `please enter the value. you are on ${entrynum} of ${count} entries.`
 
-// ])
-
+    }]).then((answers) => {
+     entrynum = entrynum + 1 
+     
+    })
+    )
+    let values = ''
 }
+
 
 
 module.exports = {addvalue}
